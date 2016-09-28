518d54de7adddc83248686183293e5b18dfd6367
oliobi <ductien1121988@gmail.com>
2016-09-28 08:45:52 +0700

    Add files via upload

---
+++ lienket.js
@@ -0,0 +1,6 @@
+jQuery('#hinhanh').html('<a href="http://datviettour.com.vn/tour-mua-thu" target="_blank" title="Chùm tour du lịch mùa thu">'+
+'<img src="http://datviettour.net/lienketchung/chum-tuor-mua-thu.jpg" alt="Chùm tour du lịch mùa thu" style="width: 100%;" />'+
+'</a><br />'+
+'<a href="http://datviettour.com.vn/tuyen-dung" target="_blank" title="Đất Việt Tour Tuyển dụng">'+
+'<img src="http://datviettour.net/lienketchung/tuyen-dung.png" alt="Đất Việt Tour Tuyển dụng" style="width: 100%;" />'+
+'</a>')
