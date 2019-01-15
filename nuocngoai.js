7a40abdf2ae26b511c11a1942dc8bdf4d05836a0
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-01-15 15:00:49 +0700

    Update nuocngoai.js

--- nuocngoai.js
+++ nuocngoai.js
@@ -1,9 +1,8 @@
-jQuery('#thaydoibanner').html(`<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-xuan-tren-tay-nguyen-4n3d-1399">
-	<img src="https://datviettour.com.vn/uploads/images/banner/tay_nguyen.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
-</a>
-<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-kham-pha-dao-tom-hum-binh-ba-2n2d-1269">
-	<img src="https://datviettour.com.vn/uploads/images/banner/binh_ba-_tay_nguyen_QC-01.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
-</a>
+jQuery('#thaydoibanner').html
+(`
+<a href="https://datviettour.com.vn/tour-du-lich-tet-nguyen-dan-gia-re">
+	<img src="http://dulichnuocngoai.info/wp-content/uploads/2018/12/slide_tet_AL-01.png" title="Tour TẾT ÂM LỊCH" alt="Tour TẾT ÂM LỊCH" /></a>
+
 <a href="https://datviettour.com.vn/tour-du-lich-tet-2019-thai-lan-bangkok-pattaya-5n4d-kh-mung-3-4-tet-1375">
 	<img src="https://datviettour.com.vn/uploads/images/banner/slide_Out-thai_tet.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
 </a>
