63c47b2424eba8ab34197adbe7f43ba814f386bd
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-01-15 17:15:43 +0700

    Update trongnuoc.js

--- trongnuoc.js
+++ trongnuoc.js
@@ -4,9 +4,4 @@ jQuery('#thaydoibanner').html(`<a href="https://datviettour.com.vn/tour-du-lich-
 <a href="https://datviettour.com.vn/tour-du-lich-tet-2019-kham-pha-dao-tom-hum-binh-ba-2n2d-1269">
 	<img src="https://datviettour.com.vn/uploads/images/banner/binh_ba-_tay_nguyen_QC-01.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
 </a>
-<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-thai-lan-bangkok-pattaya-5n4d-kh-mung-3-4-tet-1375">
-	<img src="https://datviettour.com.vn/uploads/images/banner/slide_Out-thai_tet.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
-</a>
-<a href="https://datviettour.com.vn/">
-	<img src="https://datviettour.com.vn/uploads/images/banner/Cao_hung-_QC_trang_chu-01.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
-</a>`);
+`);
