7aabd047e821e2be7998b45d7af89f6b90ef438b
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-01-15 15:43:30 +0700

    Update nuocngoai.js

--- nuocngoai.js
+++ nuocngoai.js
@@ -6,6 +6,9 @@ jQuery('#thaydoibanner').html
 <a href="https://datviettour.com.vn/tour-du-lich-tet-2019-thai-lan-bangkok-pattaya-5n4d-kh-mung-3-4-tet-1375">
 	<img src="https://datviettour.com.vn/uploads/images/banner/slide_Out-thai_tet.png" title="Tour Thái Lan" alt="tour thai lan" style="width: 100%; padding-bottom: 10px">
 </a>
+<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-siem-riep-phnom-penh-4n3d-1407">
+	<img src="http://tourcampuchia.org/wp-content/uploads/2018/11/campuchia.jpg" title="Tour Campuchia" alt="tour campuchia" style="width: 100%; padding-bottom: 10px">
+</a>
 <a href="https://datviettour.com.vn/">
 	<img src="https://datviettour.com.vn/uploads/images/banner/Cao_hung-_QC_trang_chu-01.png" title="Tour Đài Loan" alt="tour dai loan" style="width: 100%; padding-bottom: 10px">
 </a>`);
