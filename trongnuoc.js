db0bd57651451424dadfe60f42444d847e1a4ee5
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-01-16 09:44:14 +0700

    Update trongnuoc.js

--- trongnuoc.js
+++ trongnuoc.js
@@ -1,7 +1,10 @@
 jQuery('#thaydoibanner').html(`<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-xuan-tren-tay-nguyen-4n3d-1399">
-	<img src="https://datviettour.com.vn/uploads/images/banner/tay_nguyen.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
+	<img src="https://datviettour.com.vn/uploads/images/banner/tay_nguyen.png" title="du lịch tây nguyên" alt="du lịch tây nguyên" style="width: 100%; padding-bottom: 10px">
 </a>
-<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-kham-pha-dao-tom-hum-binh-ba-2n2d-1269">
-	<img src="https://datviettour.com.vn/uploads/images/banner/binh_ba-_tay_nguyen_QC-01.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
+<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-da-lat-sac-xuan-yeu-thuong-3n3d-1393">
+	<img src="https://datviettour.com.vn/uploads/images/banner/Da_lat.png" title="du lịch đà lạt" alt="du lịch đà lạt" style="width: 100%; padding-bottom: 10px">
+</a>
+<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-phan-thiet-mui-ne-2n1d-1208">
+	<img src="https://datviettour.com.vn/uploads/images/banner/Phan_thiet.png" title= "du lịch phan thiết" alt="du lịch phan thiết" style="width: 100%; padding-bottom: 10px">
 </a>
 `);
