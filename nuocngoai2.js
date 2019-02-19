2ea58798b67d1cde7661a5f85b905c77dc57fef3
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-02-19 11:29:27 +0700

    Update nuocngoai2.js

--- nuocngoai2.js
+++ nuocngoai2.js
@@ -1,12 +1,11 @@
 jQuery('#thaydoibanner').html
 (`
-<a href="https://datviettour.com.vn/tour-du-lich-tet-nguyen-dan-gia-re">
-	<img src="http://dulichnuocngoai.info/wp-content/uploads/2018/12/slide_tet_AL-01.png" title="Tour TẾT ÂM LỊCH" alt="Tour TẾT ÂM LỊCH" /></a>
-
- <a href="https://datviettour.com.vn/tour-du-lich-tet-2019-thai-lan-bangkok-pattaya-5n4d-kh-mung-3-4-tet-1375">
-	<img src="https://datviettour.com.vn/uploads/images/banner/slide_Out-thai_tet.png" title="Tour Thái Lan" alt="tour thai lan" style="width: 100%; padding-bottom: 10px">
+ <a href="https://datviettour.com.vn/tour-du-lich-le-30-4-kham-pha-dubai-4n4d-1679">
+	<img src="https://datviettour.com.vn/uploads/images/banner/web_30-4-02.jpg" title="Tour Dubai" alt="Tour Dubai" style="width: 100%; padding-bottom: 10px">
 </a>
-
+<a href="https://datviettour.com.vn/tour-du-lich-singapore-malaysia-6n5d-kh-hang-tuan-1202">
+	<img src="https://datviettour.com.vn/uploads/images/banner/web_30-4-03.jpg" title="Tour Singapore-Malaysia" alt="Tour Singapore-Malaysia" style="width: 100%; padding-bottom: 10px">
+</a>
  <a href="https://datviettour.com.vn/tour-du-lich-dai-bac-dai-trung-gia-nghia-cao-hung-5n5d-1492">
 	<img src="https://datviettour.com.vn/uploads/images/banner/Cao_hung-_QC_trang_chu-01.png" title="Tour Đài Loan" alt="tour dai loan" style="width: 100%; padding-bottom: 10px">
 </a>`);
