76a35cde96bd7057cdcc3cf081a5dd2cf7da5e55
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-02-19 10:32:08 +0700

    Update and rename banner header blog tml to banner header blog tml.js

---
+++ banner header blog tml.js
@@ -0,0 +1,7 @@
+
+jQuery('#thaydoibanner').html
+(`
+<a href="https://datviettour.com.vn/du-lich-myanmar-kham-pha-mien-dat-phat-yangon-bago-kyaikhtiyo-golden-rock-1529">
+	<img src="https://datviettour.com.vn/uploads/images/banner/Myanmar-QC.jpg" alt="Myanmar" style="width: 100%; padding-bottom: 10px">
+</a>
+`);
