d68492452fa75f70617d67652a02152fc2268c02
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-02-19 11:25:56 +0700

    Update nuocngoai.js

--- nuocngoai.js
+++ nuocngoai.js
@@ -1,15 +1,11 @@
 jQuery('#thaydoibanner').html
 (`
-<a href="https://datviettour.com.vn/tour-du-lich-tet-nguyen-dan-gia-re">
-	<img src="http://dulichnuocngoai.info/wp-content/uploads/2018/12/slide_tet_AL-01.png" title="Tour TẾT ÂM LỊCH" alt="Tour TẾT ÂM LỊCH" /></a>
-
 <a href="https://datviettour.com.vn/tour-du-lich-nhat-ban-mua-hoa-anh-dao-nagoya-yamanashi-phu-si-tokyo-5n4d-1412">
 	<img src="https://datviettour.com.vn/uploads/images/banner/slide_Out-_hoa_dao-02.png" title="Tour Nhật Bản" alt="tour nhat ban" style="width: 100%; padding-bottom: 10px">
 </a>
-<a href="https://datviettour.com.vn/tour-du-lich-hong-kong-tham-quyen-quang-chau-sky-100-5n4d-1301">
-	<img src="https://datviettour.com.vn/uploads/images/banner/final_hong_kong_-_TQ-_QC-01.png" title="Tour Hongkong" alt="tour hongkong" style="width: 100%; padding-bottom: 10px">
+<a href="https://datviettour.com.vn/tour-du-lich-le-30-4-hong-kong-tham-quyen-quang-chau-sky-100-5n4d-1677">
+	<img src="https://datviettour.com.vn/uploads/images/banner/web_30-4-01.jpg" title="Tour Hongkong lễ 30/4" alt="tour hongkong" style="width: 100%; padding-bottom: 10px">
 </a>
-
 <a href="https://datviettour.com.vn/tour-du-lich-tet-2019-seoul-nami-everland-one-mount-park-4n4d-1329">
 	<img src="http://dulich-hanquoc.net/wp-content/uploads/2018/12/HanQuoc_OneMountPark.jpg" title="Tour Đài Loan" alt="tour dai loan" style="width: 100%; padding-bottom: 10px">
 </a>`);
