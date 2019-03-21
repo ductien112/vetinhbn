c27e90bc2ee9257dd92360c0d23c0d38c78e0af4
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-03-21 17:23:45 +0700

    Update lienket.js

--- lienket.js
+++ lienket.js
@@ -1,6 +1,7 @@
 jQuery('#thaydoibanner').html(`<a href="https://datviettour.com.vn/tour-du-lich-tet-2019-xuan-tren-tay-nguyen-4n3d-1399">
 	<img src="https://datviettour.com.vn/uploads/images/banner/tay_nguyen.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
 </a>
+
 <a href="https://datviettour.com.vn/tour-du-lich-tet-2019-kham-pha-dao-tom-hum-binh-ba-2n2d-1269">
 	<img src="https://datviettour.com.vn/uploads/images/banner/binh_ba-_tay_nguyen_QC-01.png" alt="datviettour" style="width: 100%; padding-bottom: 10px">
 </a>
