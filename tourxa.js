04c0b7a7f787c5872bc9ec2c4cd8a601eee4d578
Kdnguyen1234 <46521093+Kdnguyen1234@users.noreply.github.com>
2019-03-21 16:00:35 +0700

    Update tourxa.js

--- tourxa.js
+++ tourxa.js
@@ -1,7 +1,7 @@
 jQuery('#thaydoibanner').html
 (`
-<a href="https://datviettour.com.vn/tour-du-lich-tet-nguyen-dan-gia-re">
-	<img src="http://dulichnuocngoai.info/wp-content/uploads/2018/12/slide_tet_AL-01.png" title="Tour TẾT ÂM LỊCH" alt="Tour TẾT ÂM LỊCH" /></a>
+<a href="https://datviettour.com.vn/tour-du-lich-le-30-4">
+	<img src="http://dulichdainam.info/wp-content/uploads/2019/03/Poster1.png" title="Tour Du lịch 30/4" alt="Tour du lich 30/4" /></a>
 
  <a href="https://datviettour.com.vn/tour-du-lich-kham-pha-nuoc-uc-5n4d-1366">
 	<img src="https://datviettour.com.vn/uploads/images/chau-uc/uc/hinh-dai-dien/du-lich-uc-gia-re.jpg" title="Tour du lịch Úc giá rẻ" alt="Tour du lịch Úc giá rẻ" style="width: 100%; padding-bottom: 10px">
